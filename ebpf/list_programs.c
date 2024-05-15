7bbe179742bdea8127eecd9285cebe10147d11da
Lorenzo Cipriani <lorenzo1974@gmail.com>
2024-05-15 22:52:14 +0100

    Add ebof map list to the program

--- ebpf/list_programs.c
+++ ebpf/list_programs.c
@@ -10,13 +10,11 @@
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-/*
-Build:
-clang -Wall -o list-programs list_programs.c $(pkg-config --cflags --libs libbpf)
-*/
-int main() {
+void list_programs() {
     __u32 prog_id = 0;
     int err;
+
+    printf("Loaded eBPF Programs:\n");
 
     while (1) {
         union bpf_attr attr_next_id = {
@@ -66,6 +64,66 @@ int main() {
 
         close(prog_fd);
     }
+}
 
+void list_maps() {
+    __u32 map_id = 0;
+    int err;
+
+    printf("\nLoaded eBPF Maps:\n");
+
+    while (1) {
+        union bpf_attr attr_next_id = {
+            .start_id = map_id,
+        };
+
+        err = syscall(__NR_bpf, BPF_MAP_GET_NEXT_ID, &attr_next_id, sizeof(attr_next_id));
+        if (err) {
+            if (errno == ENOENT) {
+                break; // No more maps
+            } else {
+                perror("BPF_MAP_GET_NEXT_ID");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        map_id = attr_next_id.next_id;
+
+        union bpf_attr attr_fd_by_id = {
+            .map_id = map_id,
+        };
+
+        int map_fd = syscall(__NR_bpf, BPF_MAP_GET_FD_BY_ID, &attr_fd_by_id, sizeof(attr_fd_by_id));
+        if (map_fd < 0) {
+            perror("BPF_MAP_GET_FD_BY_ID");
+            exit(EXIT_FAILURE);
+        }
+
+        struct bpf_map_info info = {};
+        __u32 info_len = sizeof(info);
+
+        union bpf_attr attr_info = {
+            .info.bpf_fd = map_fd,
+            .info.info = (uint64_t)&info,
+            .info.info_len = info_len,
+        };
+
+        err = syscall(__NR_bpf, BPF_OBJ_GET_INFO_BY_FD, &attr_info, sizeof(attr_info));
+        if (err) {
+            perror("BPF_OBJ_GET_INFO_BY_FD");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Map ID: %u\n", map_id);
+        printf("Map Type: %u\n", info.type);
+        printf("Map Name: %s\n", info.name);
+
+        close(map_fd);
+    }
+}
+
+int main() {
+    list_programs();
+    list_maps();
     return 0;
 }
